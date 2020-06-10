c962abc0b35a6adfd43deec9fc79cec3b83d80d4
PinkuSyanpan <r2kou@outlook.com>
2020-06-10 21:39:34 +0800

    init

---
+++ js/getquerystr.js
@@ -0,0 +1,6 @@
+function getQuery(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return decodeURI(r[2]);
+    return null;
+}
